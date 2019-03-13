714b9e056e4138a0f9dd576321623436743ea243
jeetwide <37177965+jeetwide@users.noreply.github.com>
2019-03-13 17:31:30 +0530

    Added custom dialog

--- app/src/main/java/demoapp/jeetendra/Adapters/ChooseBikeAdapter.java
+++ app/src/main/java/demoapp/jeetendra/Adapters/ChooseBikeAdapter.java
@@ -1,5 +1,6 @@
 package demoapp.jeetendra.Adapters;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.graphics.Typeface;
 import android.support.v7.widget.RecyclerView;
@@ -7,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,7 +27,7 @@ public class ChooseBikeAdapter extends RecyclerView.Adapter<ChooseBikeAdapter.Vc
     private Context mCtx;
 
     //we are storing all the products in a list
-  //  private List<ModelVCI> vciList;
+    //  private List<ModelVCI> vciList;
     private List<ModelBike> bikeList;
 
     //getting the context and product list with constructor
@@ -33,6 +35,7 @@ public class ChooseBikeAdapter extends RecyclerView.Adapter<ChooseBikeAdapter.Vc
         this.mCtx = mCtx;
         this.bikeList = bikeList;
     }
+
     @Override
     public VciViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         //inflating and returning our view holder
@@ -40,6 +43,7 @@ public class ChooseBikeAdapter extends RecyclerView.Adapter<ChooseBikeAdapter.Vc
         View view = inflater.inflate(R.layout.list_row_bike, null);
         return new VciViewHolder(view);
     }
+
     @Override
     public void onBindViewHolder(VciViewHolder holder, int position) {
         //getting the product of the specified position
@@ -51,7 +55,39 @@ public class ChooseBikeAdapter extends RecyclerView.Adapter<ChooseBikeAdapter.Vc
         holder.rl_vci.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(mCtx, product.getBikename()+" clicked!!!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(mCtx, product.getBikename() + " clicked!!!", Toast.LENGTH_SHORT).show();
+
+                //Custom DIalog
+                final Dialog dialog = new Dialog(mCtx);
+                dialog.setContentView(R.layout.custom_dialog);
+                dialog.setTitle("Please choose ");
+
+                LinearLayout ll_ems = dialog.findViewById(R.id.ll_ems);
+                ll_ems.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(mCtx, "EMS Clicked!!!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                LinearLayout ll_abs = dialog.findViewById(R.id.ll_abs);
+                ll_abs.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(mCtx, "ABS Clicked!!!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+               LinearLayout ll_ic = dialog.findViewById(R.id.ll_ic);
+                ll_ic.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(mCtx, "IC Clicked!!!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                dialog.show();
+
+
             }
         });
 
@@ -70,12 +106,12 @@ public class ChooseBikeAdapter extends RecyclerView.Adapter<ChooseBikeAdapter.Vc
     class VciViewHolder extends RecyclerView.ViewHolder {
 
         //TextView textViewTitle, textViewShortDesc, textViewRating, textViewPrice;
-       RelativeLayout rl_vci;
+        RelativeLayout rl_vci;
 
         ImageView iv;
         TextView tv_bikename;
 
-        Typeface tf = Typeface.createFromAsset(itemView.getContext().getAssets(),"NexaRustSans-Black.otf");
+        Typeface tf = Typeface.createFromAsset(itemView.getContext().getAssets(), "NexaRustSans-Black.otf");
 
         public VciViewHolder(View itemView) {
             super(itemView);
@@ -90,3 +126,14 @@ public class ChooseBikeAdapter extends RecyclerView.Adapter<ChooseBikeAdapter.Vc
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
